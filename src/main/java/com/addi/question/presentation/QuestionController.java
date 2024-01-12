59a77855847c29e4eaa4f2e1b4b69c8d668fee66
kimyechan <tioon74@naver.com>
2024-01-13 06:08:23 +0900

    fix : Question 개발

---
+++ src/main/java/com/addi/question/presentation/QuestionController.java
@@ -0,0 +1,31 @@
+package com.addi.question.presentation;
+
+import com.addi.question.appication.QuestionService;
+import com.addi.question.appication.dto.QuestionResponse;
+import com.addi.stt.appication.VoiceService;
+import com.addi.translation.application.PapagoService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class QuestionController {
+
+	private final QuestionService questionService;
+
+	@GetMapping("/api/getQuestion")
+	public ResponseEntity<QuestionResponse> getQuestionResponse() {
+		QuestionResponse questionResponse = questionService.getQuestionService();
+
+		return ResponseEntity.ok(questionResponse);
+
+	}
+}
