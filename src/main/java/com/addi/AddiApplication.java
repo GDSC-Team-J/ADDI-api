55990958c12bd11e8265de91be7434e0164cf99a
Haebin <joker7011@naver.com>
2024-01-12 16:51:09 +0900

    feat: initial commit

---
+++ src/main/java/com/addi/AddiApplication.java
@@ -0,0 +1,13 @@
+package com.addi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/addi/AddiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.addi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
